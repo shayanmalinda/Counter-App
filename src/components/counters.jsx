210d0efa2b7ddddc189d5149f02f890c7141426e
shayanmalinda <shayanmalinda@gmail.com>
2020-02-25 13:57:48 +0530

    creating props variable

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,20 +3,19 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
     return (
       <div>
-        <button
-          className="btn btn-primary btn-sm m-2"
-          onClick={this.props.onReset}
-        >
+        <button className="btn btn-primary btn-sm m-2" onClick={onReset}>
           Reset
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
             counter={counter}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
           ></Counter>
         ))}
       </div>
